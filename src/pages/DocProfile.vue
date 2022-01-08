472cbd851b63a092b9deab1cf9ed26bd2bccf285
sammy-code98 <sammysmart98@yahoo.com>
2022-01-08 23:49:29 +0100

    feat: add about doctor section

--- src/pages/DocProfile.vue
+++ src/pages/DocProfile.vue
@@ -40,11 +40,11 @@
     nihil ipsa ipsam itaque rem laboriosam maiores quasi soluta amet, facere
     earum, doloribus reprehenderit nulla nostrum. Iusto, nostrum totam.
   </div>
-  <div class="text-h5 text-font q-mx-md">Working time</div>
+  <div class="text-h5 text-font q-mx-md q-mt-sm">Working time</div>
   <div class="text-font text-subtitle1 text-grey-7 q-mx-md">
     Monday - Friday (8:00am to 4:30pm)
   </div>
-  <div class="text-h5 text-font q-mx-md">Communication</div>
+  <div class="text-h5 text-font q-mx-md q-mt-sm">Communication</div>
   <div class="q-px-md">
  <div class="row q-mt-sm">
     <q-btn text-color="accent" flat round icon="mdi-chat-processing-outline" />
