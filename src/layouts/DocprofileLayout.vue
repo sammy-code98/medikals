1dce363a10ea844f874ce54a98fae11880d669c7
sammy-code98 <sammysmart98@yahoo.com>
2022-01-08 23:32:58 +0100

    feat: add doctor image and description

--- src/layouts/DocprofileLayout.vue
+++ src/layouts/DocprofileLayout.vue
@@ -2,7 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <q-header class="bg-white">
       <q-toolbar>
-        <q-btn flat round dense icon="mdi-less-than" color="black" />
+        <q-btn flat round dense icon="mdi-less-than" color="black" to="/dashboard" />
         <q-space />
         <q-btn flat round dense icon="mdi-dots-vertical" color="black" />
       </q-toolbar>
@@ -11,7 +11,13 @@
     <!-- footer menu -->
     <q-footer class="bg-white" bordered>
       <q-card flat square class="q-pa-md">
-        <q-btn color="primary" label="Make an Appointment" class="full-width  text-font text-subtitle1~" dense no-caps  />
+        <q-btn
+          color="primary"
+          label="Make an Appointment"
+          class="full-width text-font text-subtitle1~"
+          dense
+          no-caps
+        />
       </q-card>
     </q-footer>
 
--- src/pages/DocProfile.vue
+++ src/pages/DocProfile.vue
@@ -1,3 +1,42 @@
 <template>
-    <div>helele</div>
+  <div class="row q-pa-md">
+    <div class="col-5">
+      <q-img
+        src="https://cdn.quasar.dev/img/boy-avatar.png"
+        class="rounded-borders"
+        height="230px"
+      />
+    </div>
+    <div class="col"></div>
+    <div class="col-6">
+      <div class="text-font text-accent text-h6">Dr. Emma Uchewa</div>
+      <div class="text-font text-grey-7 text-subtitle1">Dentist</div>
+      <div class="row">
+        <q-btn text-color="yellow" flat round icon="mdi-star" />
+        <div class="q-mx-md">
+          <div class="text-font text-grey-7 text-subtitle2">Rating</div>
+          <div class="text-font text-accent text-subtitle1">5.0 out of 5</div>
+        </div>
+      </div>
+      <div class="row q-mt-sm">
+        <q-btn text-color="primary" flat round icon="mdi-hospital-building" />
+        <div class="q-mx-md">
+          <div class="text-font text-grey-7 text-subtitle2">Patients</div>
+          <div class="text-font text-accent text-subtitle1">2500</div>
+        </div>
+      </div>
+      <div class="row q-mt-sm">
+        <q-btn
+          text-color="accent"
+          flat
+          round
+          icon="mdi-timeline"
+        />
+        <div class="q-mx-md">
+          <div class="text-font text-grey-7 text-subtitle2">Experience</div>
+          <div class="text-font text-accent text-subtitle1">20 years</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
