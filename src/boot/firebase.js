4553d846fe2271a103075c767a91493801e8cf3a
sammy-code98 <sammysmart98@yahoo.com>
2022-07-25 21:14:24 +0100

    install and configure firebase

---
+++ src/boot/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAB6uEADnUGhqvYJ-NVnLo-0P3YhwtvHI0",
+  authDomain: "medicals-8921f.firebaseapp.com",
+  projectId: "medicals-8921f",
+  storageBucket: "medicals-8921f.appspot.com",
+  messagingSenderId: "7912751222",
+  appId: "1:7912751222:web:3a1d9d52e5aaac51095f57",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
