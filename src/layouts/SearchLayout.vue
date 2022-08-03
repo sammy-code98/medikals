d3c6f3e4de5cd88826503095196092976ad0af86
sammy-code98 <sammysmart98@yahoo.com>
2022-08-03 20:20:13 +0100

    space out content on doc profile

--- src/layouts/SearchLayout.vue
+++ src/layouts/SearchLayout.vue
@@ -17,6 +17,30 @@
       </q-toolbar>
     </q-header>
 
+    <!-- footer menu -->
+    <q-footer class="bg-white" bordered>
+      <q-card flat square>
+        <q-tabs
+          class="text-grey-7"
+          active-class="text-accent"
+          indicator-color="transparent"
+        >
+          <q-route-tab icon="mdi-home" to="/dashboard" />
+
+          <q-route-tab icon="mdi-chat-processing-outline" exact to="/chats" />
+          <q-route-tab icon="search" exact to="/search" />
+
+          <q-route-tab
+            icon="mdi-calendar-month-outline"
+            to="/appointments"
+            exact
+          />
+
+          <q-route-tab icon="mdi-account-outline" exact to="/profile" />
+        </q-tabs>
+      </q-card>
+    </q-footer>
+
     <q-page-container>
       <router-view />
     </q-page-container>
