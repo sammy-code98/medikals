dedf44ab18563f478245743b229dcf1c36e39a12
sammy-code98 <sammysmart98@yahoo.com>
2022-08-19 13:24:11 +0100

    feat: add make appointment dialog

--- src/layouts/DocprofileLayout.vue
+++ src/layouts/DocprofileLayout.vue
@@ -27,8 +27,57 @@
           class="full-width text-font text-subtitle1~"
           dense
           no-caps
+          @click="prompt = true"
         />
       </q-card>
+
+      <q-dialog v-model="prompt" persistent>
+        <q-card style="min-width: 350px">
+          <q-card-section>
+            <div class="text-h6 text-accent text-center">
+              Make An Appointment
+            </div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            <q-input
+              dense
+              outlined
+              class="text-font text-subtitle1 q-mb-md"
+              v-model="docName"
+            />
+
+            <q-input
+              dense
+              outlined
+              class="text-font text-subtitle1 q-mb-md"
+              v-model="docField"
+            />
+
+            <q-input
+              dense
+              outlined
+              autofocus
+              v-model="date"
+              class="text-font text-subtitle1 q-mb-md"
+              type="date"
+              hint="Pick an Appointment Date"
+            />
+            <q-input
+              dense
+              outlined
+              v-model="time"
+              type="time"
+              hint="Pick an Appointment Time"
+            />
+          </q-card-section>
+
+          <q-card-actions align="right" class="text-primary">
+            <q-btn flat label="Cancel" v-close-popup />
+            <q-btn flat label="Add Appointment" v-close-popup />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
     </q-footer>
 
     <q-page-container>
@@ -38,10 +87,41 @@
 </template>
 
 <script>
+import { ref, onMounted, watchEffect } from "vue";
+import { useRoute } from "vue-router";
+
 export default {
   name: "MainLayout",
   setup() {
-    return {};
+    const route = useRoute();
+    let name = ref("");
+    let field = ref("");
+
+    function getName() {
+      name.value = route.params.profile;
+    }
+
+    function getField() {
+      field.value = route.params.speciality;
+    }
+    onMounted(() => {
+      getName();
+      getField();
+    });
+    watchEffect(() => {
+      getName();
+      getField();
+    });
+    return {
+      prompt: ref(false),
+      name,
+      date: ref(""),
+      time: ref(""),
+      docName: ref(name),
+      docField: ref(field),
+      getName,
+      getField,
+    };
   },
 };
 </script>
